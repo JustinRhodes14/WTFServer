e474eecf80f5ba806f5a1fc0e3fc69e8198a3440
JustinRhodes14 <46693438+JustinRhodes14@users.noreply.github.com>
2020-04-13 20:02:00 -0400

    Update WTF.c

--- WTF.c
+++ WTF.c
@@ -5,27 +5,7 @@
 #include <sys/socket.h> 
 #define MAX 80 
 #define SA struct sockaddr 
-void func(int sockfd) 
-{ 
-    char buff[MAX]; 
-    int n; 
-    for (;;) { 
-        bzero(buff, sizeof(buff)); 
-        printf("Enter the string : "); 
-        n = 0; 
-        while ((buff[n++] = getchar()) != '\n') 
-            ; 
-        write(sockfd, buff, sizeof(buff)); 
-        bzero(buff, sizeof(buff)); 
-        read(sockfd, buff, sizeof(buff)); 
-        printf("From Server : %s", buff); 
-        if ((strncmp(buff, "exit", 4)) == 0) { 
-            printf("Client Exit...\n"); 
-            break; 
-        } 
-    } 
-} 
-  
+
 int main(int argc, char** argv) 
 { 
 	if (argc == 4) {
@@ -67,7 +47,35 @@ int main(int argc, char** argv)
     // close the socket 
     close(sockfd); 
 }
-
+int compareString(char* str1, char* str2) {
+	int len1 = strlen(str1);
+	int len2 = strlen(str2);
+	int shorter = 0;
+	int len = len1;
+	if (len1 > len2) {
+		shorter = 1;//first one is shorter
+		len = len1;
+	} else if (len2 < len1) {
+		shorter = 2;//second one is shorter
+		len = len2;
+	} else {
+		shorter = 0;//equal length
+		len = len1;
+	}
+	
+	int i;
+	for ( i = 0; i < len; i++) {
+		if (str1[i] != str2[i]) {
+			return ((int)str1[i] - (int)str2[i]);//negative if str1 is lesser, positive if str1 is greater
+		}	
+	}
+	if (len1 < len2) {
+		return -1;
+	} else if (len2 < len1) {
+		return 1;
+	}
+	return 0;//equal strings
+}
 void configure(char* ip, char* port) {
 	int fd;
 	fd = open ("./.configure", O_RDWRONLY | O_CREAT | O_TRUNC,00600);
@@ -75,6 +83,27 @@ void configure(char* ip, char* port) {
 	writeTo(fd,"\n\0");
 	writeTo(fd,port);
 }
+
+void func(int sockfd) 
+{ 
+    char buff[MAX]; 
+    int n; 
+    for (;;) { 
+        bzero(buff, sizeof(buff)); 
+        printf("Enter the string : "); 
+        n = 0; 
+        while ((buff[n++] = getchar()) != '\n') 
+            ; 
+        write(sockfd, buff, sizeof(buff)); 
+        bzero(buff, sizeof(buff)); 
+        read(sockfd, buff, sizeof(buff)); 
+        printf("From Server : %s", buff); 
+        if ((strncmp(buff, "exit", 4)) == 0) { 
+            printf("Client Exit...\n"); 
+            break; 
+        } 
+    } 
+} 
 
 void writeTo(int fd, char* word) {
 	int bytesWritten = 0;
